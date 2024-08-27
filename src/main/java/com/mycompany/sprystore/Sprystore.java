31f6e1213723544793a62db67383140fc336ae81
Anointing <anointing@kali>
2024-08-27 13:48:04 -0400

    first commit

---
+++ src/main/java/com/mycompany/sprystore/Sprystore.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sprystore;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author anointing
+ */
+public class Sprystore {
+
+    public static void main(String[] args) {
+        
+    
+//    System.out.println(helloworld);
+    
+    
+    
+//    Taking Input From User
+    
+//    Scanner Anointing = new Scanner(System.in);
+//    System.out.println("Hi am cotana, what is your name ?");
+//    String names = Anointing.nextLine();
+//    
+//    System.out.println("Hello " + names + ", nice meeting you");
+   
+    
+    
+//    Take user fullname 10:10 -> 10:30
+
+//     Scanner Baffour = new Scanner(System.in);
+//     System.out.println("Hi am Anointing, What is your firstname");
+//     String name1 = Baffour.nextLine();
+//     System.out.println("What is your surname");
+//     String name2 = Baffour.nextLine();
+//     
+//     System.out.println("Hello " + name1 + name2 + ", nice meeting you");
+     
+
+
+       
+//    Calculator +,-,*,/
+
+//      Scanner CalInput = new Scanner(System.in);
+//      
+//      System.out.println("Input First Number");
+//      float FirstNumber = CalInput.nextFloat();
+//      
+//      System.out.println("Input Second Number");
+//      float SecondNumber = CalInput.nextFloat();
+//      
+//      System.out.println("The sum of " + FirstNumber + " + " + SecondNumber + " == " + (FirstNumber + SecondNumber));
+//      System.out.println("The product of " + FirstNumber + " * " + SecondNumber + " == " + FirstNumber * SecondNumber);
+      
+       
+        Scanner CalInput = new Scanner(System.in);
+        
+        System.out.println("Input First Number");
+        float FirstNumber = CalInput.nextFloat();
+        
+        System.out.println("Input Second Number");
+        float SecondNumber = CalInput.nextFloat();
+        
+        System.out.println("The Difference of " + FirstNumber + " - " + SecondNumber + " == " + (FirstNumber - SecondNumber));
+        
+    
+
+
+    }
+    
+    public static void helloworld(){
+        
+        System.out.println("Hello World!");
+    
+    }
+    
+}
